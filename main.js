81c14856b83898d41a10e0f88d670ef23e13dfa3
BroBQ <mateusz121518@gmail.com>
2019-03-28 14:50:42 +0100

    styling for mouseover

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ const refresh = () => {
 }
 
 resetButton.addEventListener("click", refresh)
+
+const mouseover = function () {
+	const element = this.children;
+	for (let index = 0; index < element.length; index++) {
+		element[index].style.backgroundColor = "yellow";
+		element[index].style.fontWeight = "bold";
+	}
+}
+const mouseout = function () {
+	const element = this.children;
+	for (let index = 0; index < element.length; index++) {
+		element[index].style.backgroundColor = "";
+		element[index].style.fontWeight = "";
+	}
+}
+
+const allTr = document.querySelectorAll("tr");
+allTr.forEach(element => {
+	element.addEventListener("mouseover", mouseover)
+	element.addEventListener("mouseout", mouseout)
+});
