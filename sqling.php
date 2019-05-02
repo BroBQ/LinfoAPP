0c69878470bdce064d394288ddcfd8dc4b68655f
BroBQ <mateusz121518@gmail.com>
2019-05-02 13:38:13 +0200

    ready sql php

--- sqling.php
+++ sqling.php
@@ -5,6 +5,12 @@ $username = "root";
 $password = "";
 $dbname = "linfoapp";
 
+$databaseConnection = NULL;
+
+$sqlSystem = '';
+$sqlCPU = '';
+$sqlRAM = '';
+$netDevicesDataSQL = array();
 
 function connect() {
 	$databaseConnection = new mysqli($servername, $username, $password, $dbname);
@@ -13,48 +19,42 @@ function connect() {
 	}
 }
 
-//creating sql for system table
-$sqlSystem = 'INSERT INTO `system` (`Hostname`, `Processes`, `Threads`, `SystemLoad`, `Uptime`, `SystemDate`) VALUES ("' . $parser['HostName'] . '","' . $parser['processStats']['proc_total'] . '","' . $parser['processStats']['threads'] . '","' . $parser['Load'] . '","' . $parser['UpTime']['text'] . '", NOW())';
+function createSQL($parser) {
+	$sqlSystem = 'INSERT INTO `system` (`Hostname`, `Processes`, `Threads`, `SystemLoad`, `Uptime`, `SystemDate`) VALUES ("' . $parser['HostName'] . '","' . $parser['processStats']['proc_total'] . '","' . $parser['processStats']['threads'] . '","' . $parser['Load'] . '","' . $parser['UpTime']['text'] . '", NOW())';
 
 
-$MHzSum = 0;
-$usageSum = 0;
-for ($i=0; $i < count($parser["CPU"]); $i++) { 
-	$MHzSum += $parser["CPU"][$i]["MHz"];
-	$usageSum += $parser["CPU"][$i]["usage_percentage"];
-}
-$avgMHz = $MHzSum / count($parser["CPU"]);
-$avgUsage = $usageSum / count($parser["CPU"]);
-$sqlCPU = 'INSERT INTO `cpuinfo` (`Model`, `MHz`, `UsagePercentage`, `CPUDate`) VALUES ("' . $parser["CPU"][0]["Model"] . '","' . $avgMHz . '","' . $avgUsage . '", NOW())';
+	$MHzSum = 0;
+	$usageSum = 0;
+	for ($i=0; $i < count($parser["CPU"]); $i++) { 
+		$MHzSum += $parser["CPU"][$i]["MHz"];
+		$usageSum += $parser["CPU"][$i]["usage_percentage"];
+	}
+	$avgMHz = $MHzSum / count($parser["CPU"]);
+	$avgUsage = $usageSum / count($parser["CPU"]);
+	$sqlCPU = 'INSERT INTO `cpuinfo` (`Model`, `MHz`, `UsagePercentage`, `CPUDate`) VALUES ("' . $parser["CPU"][0]["Model"] . '","' . $avgMHz . '","' . $avgUsage . '", NOW())';
+	
 
-$sqlRAM = "INSERT INTO `ram` (`Total`, `Free`, `RAMDate`) VALUES (" . $parser["RAM"]["total"] . "," . $parser["RAM"]["free"] . ", NOW())";
-
-
-
-$netDevicesDataSQL = array();
-foreach ($parser["Network Devices"] as $key => $value) {
-	$a = array($key, $value["recieved"]["bytes"], $value["sent"]["bytes"], $value["state"], $value["type"]);
-	$netSQL = 'INSERT INTO `networkdevices` (`Name`, `ReceivedBytes`, `SentBytes`, `Status`, `DeviceType`, `NetworkDate`) VALUES ("' . $a[0] . '","' . $a[1] . '","' . $a[2] . '","' . $a[3] . '","' . $a[4] . '", NOW())';
-
-	array_push($netDevicesDataSQL, $netSQL);
-}
-
-
-
-
-
-foreach ($parser["Mounts"] as $key => $value) {	
-	echo "<tr><td id='devtype'>" . $value["devtype"] . "</td><td>" . $value["mount"] . "</td><td>" . $value["label"] . "</td><td>" . $value["type"] . "</td><td>" . $value["size"] . "</td><td>" . $value["used"] . "</td><td>" . $value["free"] . "</td></tr>";	
+	$sqlRAM = "INSERT INTO `ram` (`Total`, `Free`, `RAMDate`) VALUES (" . $parser["RAM"]["total"] . "," . $parser["RAM"]["free"] . ", NOW())";
+	
+	
+	foreach ($parser["Network Devices"] as $key => $value) {
+		$a = array($key, $value["recieved"]["bytes"], $value["sent"]["bytes"], $value["state"], $value["type"]);
+		$netSQL = 'INSERT INTO `networkdevices` (`Name`, `ReceivedBytes`, `SentBytes`, `Status`, `DeviceType`, `NetworkDate`) VALUES ("' . $a[0] . '","' . $a[1] . '","' . $a[2] . '","' . $a[3] . '","' . $a[4] . '", NOW())';
+	
+		array_push($netDevicesDataSQL, $netSQL);
+	}		
 }
 
-$databaseConnection->query($sqlSystem);
-$databaseConnection->query($sqlCPU);
-$databaseConnection->query($sqlRAM);
-foreach ($netDevicesDataSQL as $key => $value) {
-	$databaseConnection->query($value);
+function send() {
+	$databaseConnection->query($sqlSystem);
+	$databaseConnection->query($sqlCPU);
+	$databaseConnection->query($sqlRAM);
+	foreach ($netDevicesDataSQL as $key => $value) {
+		$databaseConnection->query($value);
+	}
 }
 
-$databaseConnection->close();
-
-
+function closeConnection() {
+	$databaseConnection->close();
+}
 ?>
