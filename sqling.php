d0d55fbdf879c626912ba5c137493f2904b6fd18
BroBQ <mateusz121518@gmail.com>
2019-05-02 13:27:07 +0200

    moved sql from app to sqling

---
+++ sqling.php
@@ -0,0 +1,55 @@
+<?php
+//connecting database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "linfoapp";
+$databaseConnection = new mysqli($servername, $username, $password, $dbname);
+if ($databaseConnection->connect_error) {
+    die("Connection failed: " . $databaseConnection->connect_error);
+}
+
+//creating sql for system table
+$sqlSystem = 'INSERT INTO `system` (`Hostname`, `Processes`, `Threads`, `SystemLoad`, `Uptime`, `SystemDate`) VALUES ("' . $parser['HostName'] . '","' . $parser['processStats']['proc_total'] . '","' . $parser['processStats']['threads'] . '","' . $parser['Load'] . '","' . $parser['UpTime']['text'] . '", NOW())';
+
+
+$MHzSum = 0;
+$usageSum = 0;
+for ($i=0; $i < count($parser["CPU"]); $i++) { 
+	$MHzSum += $parser["CPU"][$i]["MHz"];
+	$usageSum += $parser["CPU"][$i]["usage_percentage"];
+}
+$avgMHz = $MHzSum / count($parser["CPU"]);
+$avgUsage = $usageSum / count($parser["CPU"]);
+$sqlCPU = 'INSERT INTO `cpuinfo` (`Model`, `MHz`, `UsagePercentage`, `CPUDate`) VALUES ("' . $parser["CPU"][0]["Model"] . '","' . $avgMHz . '","' . $avgUsage . '", NOW())';
+
+
+
+
+$netDevicesDataSQL = array();
+foreach ($parser["Network Devices"] as $key => $value) {
+	$a = array($key, $value["recieved"]["bytes"], $value["sent"]["bytes"], $value["state"], $value["type"]);
+	$netSQL = 'INSERT INTO `networkdevices` (`Name`, `ReceivedBytes`, `SentBytes`, `Status`, `DeviceType`, `NetworkDate`) VALUES ("' . $a[0] . '","' . $a[1] . '","' . $a[2] . '","' . $a[3] . '","' . $a[4] . '", NOW())';
+
+	array_push($netDevicesDataSQL, $netSQL);
+}
+
+
+
+
+
+foreach ($parser["Mounts"] as $key => $value) {	
+	echo "<tr><td id='devtype'>" . $value["devtype"] . "</td><td>" . $value["mount"] . "</td><td>" . $value["label"] . "</td><td>" . $value["type"] . "</td><td>" . $value["size"] . "</td><td>" . $value["used"] . "</td><td>" . $value["free"] . "</td></tr>";	
+}
+
+$databaseConnection->query($sqlSystem);
+$databaseConnection->query($sqlCPU);
+$databaseConnection->query($sqlRAM);
+foreach ($netDevicesDataSQL as $key => $value) {
+	$databaseConnection->query($value);
+}
+
+$databaseConnection->close();
+
+
+?>
