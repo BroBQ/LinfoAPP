400cf0aebc74be59a8e045e79275e0ce27a3465b
BroBQ <mateusz121518@gmail.com>
2019-05-02 13:55:29 +0200

    fixes for file

--- sqling.php
+++ sqling.php
@@ -14,14 +14,14 @@ class Sqling {
 	private $netDevicesDataSQL = array();
 
 	function connect() {
-		$databaseConnection = new mysqli($servername, $username, $password, $dbname);
-		if ($databaseConnection->connect_error) {
-			die("Connection failed: " . $databaseConnection->connect_error);
+		$this->databaseConnection = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+		if ($this->databaseConnection->connect_error) {
+			die("Connection failed: " . $this->databaseConnection->connect_error);
 		}
 	}
 	
 	function createSQL($parser) {
-		$sqlSystem = 'INSERT INTO `system` (`Hostname`, `Processes`, `Threads`, `SystemLoad`, `Uptime`, `SystemDate`) VALUES ("' . $parser['HostName'] . '","' . $parser['processStats']['proc_total'] . '","' . $parser['processStats']['threads'] . '","' . $parser['Load'] . '","' . $parser['UpTime']['text'] . '", NOW())';
+		$this->sqlSystem = 'INSERT INTO `system` (`Hostname`, `Processes`, `Threads`, `SystemLoad`, `Uptime`, `SystemDate`) VALUES ("' . $parser['HostName'] . '","' . $parser['processStats']['proc_total'] . '","' . $parser['processStats']['threads'] . '","' . $parser['Load'] . '","' . $parser['UpTime']['text'] . '", NOW())';
 	
 	
 		$MHzSum = 0;
@@ -32,31 +32,31 @@ class Sqling {
 		}
 		$avgMHz = $MHzSum / count($parser["CPU"]);
 		$avgUsage = $usageSum / count($parser["CPU"]);
-		$sqlCPU = 'INSERT INTO `cpuinfo` (`Model`, `MHz`, `UsagePercentage`, `CPUDate`) VALUES ("' . $parser["CPU"][0]["Model"] . '","' . $avgMHz . '","' . $avgUsage . '", NOW())';
+		$this->sqlCPU = 'INSERT INTO `cpuinfo` (`Model`, `MHz`, `UsagePercentage`, `CPUDate`) VALUES ("' . $parser["CPU"][0]["Model"] . '","' . $avgMHz . '","' . $avgUsage . '", NOW())';
 		
 	
-		$sqlRAM = "INSERT INTO `ram` (`Total`, `Free`, `RAMDate`) VALUES (" . $parser["RAM"]["total"] . "," . $parser["RAM"]["free"] . ", NOW())";
+		$this->sqlRAM = "INSERT INTO `ram` (`Total`, `Free`, `RAMDate`) VALUES (" . $parser["RAM"]["total"] . "," . $parser["RAM"]["free"] . ", NOW())";
 		
 		
 		foreach ($parser["Network Devices"] as $key => $value) {
 			$a = array($key, $value["recieved"]["bytes"], $value["sent"]["bytes"], $value["state"], $value["type"]);
 			$netSQL = 'INSERT INTO `networkdevices` (`Name`, `ReceivedBytes`, `SentBytes`, `Status`, `DeviceType`, `NetworkDate`) VALUES ("' . $a[0] . '","' . $a[1] . '","' . $a[2] . '","' . $a[3] . '","' . $a[4] . '", NOW())';
 		
-			array_push($netDevicesDataSQL, $netSQL);
+			array_push($this->netDevicesDataSQL, $netSQL);
 		}		
 	}
 	
 	function send() {
-		$databaseConnection->query($sqlSystem);
-		$databaseConnection->query($sqlCPU);
-		$databaseConnection->query($sqlRAM);
-		foreach ($netDevicesDataSQL as $key => $value) {
-			$databaseConnection->query($value);
+		$this->databaseConnection->query($this->sqlSystem);
+		$this->databaseConnection->query($this->sqlCPU);
+		$this->databaseConnection->query($this->sqlRAM);
+		foreach ($this->netDevicesDataSQL as $key => $value) {
+			$this->databaseConnection->query($value);
 		}
 	}
 	
 	function closeConnection() {
-		$databaseConnection->close();
+		$this->databaseConnection->close();
 	}
 }
 
