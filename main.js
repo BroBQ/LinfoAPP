8fc24e67c17b6d523cb7415e76f45ce9bbbf2c52
BroBQ <mateusz121518@gmail.com>
2019-06-04 18:47:52 +0200

    functions for json feature

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function init() {
+	loadJSON(function(response) {
+	 // Parse JSON string into object
+	   var actual_JSON = JSON.parse(response);
+	});
+}
+
 const resetButton = document.createElement("button")
 resetButton.textContent = "Refresh page"
 document.body.appendChild(resetButton)
@@ -28,6 +35,20 @@ allTr.forEach(element => {
 	element.addEventListener("mouseover", mouseover)
 	element.addEventListener("mouseout", mouseout)
 });
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'config.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
 
 let size = "";
 
